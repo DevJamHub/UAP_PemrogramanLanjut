d5fc97c95456132dce24398e6afbfad91d2646c6
DevJam <sigitnovriyanto002@gmail.com>
2025-12-25 22:46:40 +0700

    ini testing

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ import ui.MainFrame;
 
 import javax.swing.*;
 
+//fungsi eksekusi program
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
